e5128cbea19cba46bbf1314c2ed3174028a7617f
Dima Snisarenko <snisarenkodima@gmail.com>
2018-12-15 10:42:14 +0100

    Add ionic 4 app with capacitor and ionic-native-http-connection-backend

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private httpClient: HttpClient
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+
+    this.httpClient.get('https://httpbin.org/get').subscribe((data) => {
+      console.log('Data', data);
+    });
+  }
+}
