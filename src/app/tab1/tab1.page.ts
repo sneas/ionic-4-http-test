67455d47680c68500faa34e73998a83017f2439c
Dima Snisarenko <snisarenkodima@gmail.com>
2019-12-01 09:27:09 +0100

    feat: perform request

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,26 +1,29 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
+    constructor(private httpClient: HttpClient) {
+        this.httpClient
+            .request(
+                new HttpRequest(
+                    'GET',
+                    'https://httpbin.org/redirect-to?url=https%3A%2F%2Fhttpbin.org%2Fget%3Fa%3Dc&status_code=301',
+                    {
+                        headers: new HttpHeaders({
+                            'content-type': 'text/plain',
+                        }),
+                    },
+                ),
+            )
+            .subscribe(data => {
+                console.log('Data', data);
+            });
+    }
 
-  constructor(
-      private httpClient: HttpClient,
-  ) {
-    this.httpClient.get('https://httpbin.org/get?a=b', {
-      headers: new HttpHeaders({
-        'content-type': 'text/plain'
-      })
-    }).subscribe((data) => {
-      console.log('Data', data);
-    });
-  }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
